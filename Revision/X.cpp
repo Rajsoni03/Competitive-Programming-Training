c6b142ab39d73373fd1ef9388373fbf2e5ba9681
Rajkumar Soni <rajksoni029@gmail.com>
2023-06-05 22:00:01 +0530

    code added

--- Revision/X.cpp
+++ Revision/X.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 
-// 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+// 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
 // n = 10
-// 1, 2, 3 ......... n-2, n-1, n
+// n, n-1, n-2, n-3, ....... 2, 1
+
+
+
 
 void func(int n){
-	if (n==0) return; // > 0
-	func(n-1); // n-1
+	if (n==0) return;  // > 0
 	cout << n << endl; // n
+	func(n-1); 		   // n-1
 }
 
 int main(){
--- Revision/Y.cpp
+++ Revision/Y.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
 
+
 // 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 // n = 10
 // 1, 2, 3 ......... n-2, n-1, n
 
-void func(int n){
-	if (n==0) return; // > 0
-	func(n-1); // n-1
-	cout << n << endl; // n
+void func(int i, int n){
+	if (i>n) return; 
+	cout << i << endl; 
+	func(i+1, n);
 }
 
 int main(){
-	func(10);
+	func(1, 20);
 	return 0;
 }
